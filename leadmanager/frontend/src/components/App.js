438d75d884aa5a1853e8010650409be2a0af891c
khairul-abdi <kabdi384@gmail.com>
2020-05-27 13:31:00 +0700

    Error Handling & Alerts

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,18 +1,36 @@
 import React, { Component, Fragment } from 'react'
 import ReactDom from 'react-dom'
 
-import Header from './layouts/Header'
+import { positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
+
+import Header from './layout/Header'
 import Dashboard from './leads/Dashboard'
+import Alerts from './layout/Alerts'
+import { Provider } from 'react-redux'
+
+import store from '../store'
+
+// Alert Options
+const alertOptions = {
+  timeout: 3000,
+  position: positions.TOP_CENTER,
+}
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Header />
-        <div className='container'>
-          <Dashboard />
-        </div>
-      </Fragment>
+      <Provider store={store}>
+        <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Fragment>
+            <Header />
+            <Alerts />
+            <div className='container'>
+              <Dashboard />
+            </div>
+          </Fragment>
+        </AlertProvider>
+      </Provider>
     )
   }
 }
